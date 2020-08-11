1e4dd2373a5973367276aecd9234461d5d18afca
radoslaw <sajdak,radoslaw@gmail.com>
2020-08-11 23:28:17 +0200

    working libraries. Can use SoftwareSerial

--- main.cpp
+++ main.cpp
@@ -4,53 +4,54 @@
  * Created: 2020-08-11 14:31:06
  * Author : Sajdak
  */ 
-#ifndef F_CPU
-#define F_CPU 16000000UL
-#endif
 
 #include <avr/io.h>
 #include "avr/interrupt.h"
 #include "stdio.h"
 #include "avr/delay.h"
-#include "uart.h"
 
-#define BUFF_SIZE		25
+#include "SoftwareSerial.h"
+#include "Sim800l.h"
+
+Sim800l SIM;
+SoftwareSerial mySerial(5, 4); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+
 
 
 int main(void)
 {
+	mySerial.begin(9600);
 	DDRB |= (1 << PINB5);
 	
-	EICRA = (1 << ISC01) ;
-	EIMSK = (1 << INT0);
-	PORTD |= (1 << PIND2);
+	EICRA = (1 << ISC11) | (1 << ISC01) ;
+	EIMSK = (1 << INT1) | (1 << INT0);
+	PORTD |= (1 << PIND3) | (1 << PIND2);
 	sei();
     /* Replace with your application code */
-	uart0_init(UART_BAUD_SELECT(9600,F_CPU));
-	uint16_t buffer[BUFF_SIZE];
-	
 	
 	
 	
     while (1) 
     {
-		if (uart0_available() != 0)
-		{
-			uint8_t number = uart0_available();
-			for (int i = 0 ; i < number; i++)
-			{
-				buffer[i] = uart0_getc();
-			}
-		}
+		;
     }
 	return 0;
 }
 ISR(INT0_vect)
 {
 	cli();
-	_delay_ms(600);
+	_delay_ms(1000);
 	PORTB ^= (1 << PINB5);
-	uart0_puts("ATD+\"+48664059986\"");
+
+	mySerial.println("ATD+ +48793380333;");
 	
+	sei();
+}
+
+ISR(INT1_vect)
+{
+	cli();
+	_delay_ms(500);
+	mySerial.println("ATH");
 	sei();
 }
